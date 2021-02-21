82b77f2e87229eb32202bcfb11749a18a2b379c9
Igork <igork@korentec.co.il>
2021-02-21 18:21:04 +0200

    init rest api

---
+++ src/main/java/com/services/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
