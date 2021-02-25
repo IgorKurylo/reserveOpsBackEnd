fa1c86f57cd7a77f2d7ca4ce89229b62b5802bc7
Igork <igork@korentec.co.il>
2021-02-25 21:28:50 +0200

    adding db connection and adding config

--- src/main/java/repository/DatabaseConnection.java
+++ src/main/java/repository/DatabaseConnection.java
@@ -1,9 +1,44 @@
 package repository;
 
-public class DatabaseConnection {
-    public DatabaseConnection() {
-    }
-    public void establishConnection(){
+import application.ApplicationConfig;
 
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    @Inject
+    ApplicationConfig config;
+    private String url;
+    private String user;
+    private String password;
+    Connection connection = null;
+
+    public DatabaseConnection() {
+        url = config.getValue("connection_string");
+        user = config.getValue("db_user");
+        password = config.getValue("db_password");
+
+    }
+
+    public void openConnection() {
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.printf("Connected to %s",url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
